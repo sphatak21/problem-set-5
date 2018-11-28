7897c74fa1fb7a957ac695fb1ec29dca4eebba8b
sphatak21 <siddphatak@gmail.com>
2018-11-28 10:53:16 -0500

    finished 3,4and5

--- js/problem-set-5.js
+++ js/problem-set-5.js
@@ -165,15 +165,50 @@ function credit() {
   //////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 3 CODE HERE
-  card=prompt("Enter your credit card number: ");
-  
+  let odd_sum=0;
+  let even_sum=0;
+  while (true){
+    card=prompt("Enter your credit card number: ");
+    if ((card.length==16 || card.length==15 || card.length==13) && Number.isInteger(Number(card))){
+      break;
+    }
+  }
+  for(let i=card.length-2;i>=0;i-=2) {
+    let num=Number(card[i])*2;
+    let strnum=num.toString();
+    let sum_num=0;
+    for (let j=0;j<strnum.length;j++){
+      sum_num=sum_num+Number(strnum[j]);
+    }
+    even_sum=sum_num+even_sum;
+    console.log(even_sum);
+  }
+  for(let k=card.length-1; k>=0;k-=2){
+    odd_sum=odd_sum+Number(card[k])
+  }
+  console.log(odd_sum);
+
+  if (card.length==15 && (card[0]==3 &&(card[1]==7 || card[1]==4)) && (odd_sum+even_sum)%10==0){
+    document.getElementById("credit-output").innerHTML="<img src ='./images/amex.png'/>";
+  }
+  else if ((card.length==13 || card.length==16) && card[0]==4 && (odd_sum+even_sum)%10==0){
+    document.getElementById("credit-output").innerHTML="<img src ='./images/visa.png'/>";
+  }
+  else if (card.length==16 && (card[0]==5 && (card[1]==1 || card[1]==2 || card[1]==4 || card[1]==5)) && (odd_sum+even_sum)%10==0){
+    document.getElementById("credit-output").innerHTML="<img src ='./images/mastercard.png'/>";
+  }
+  else {
+    document.getElementById("credit-output").innerHTML="<img src ='./images/invalid.png'/>";
+  }
+
+  card=Number(card);
+
   /*
    * NOTE: After reading in the card number and storing it in the 'card'
    *       variable, do not modify it. If you find it necessary to manipulate
    *       this value, you will need to create a second variable to serve
    *       as a copy of the 'card' variable.
    */
-   document.getElementById("credot").innerHTML='1';
   ///////////////////////// DO NOT MODIFY
   check('credit', card); // DO NOT MODIFY
   ///////////////////////// DO NOT MODIFY
@@ -205,6 +240,29 @@ function credit() {
 function guess() {
 
   // WRITE YOUR EXERCISE 4 CODE HERE
+  let number=Math.floor(Math.random()*999)+1;
+  let attempts=0;
+  let correct_answer = false;
+  while (correct_answer==false) {
+    let guess=prompt('enter your guess')
+    if(guess>=1 && guess<=1000 && Number.isInteger(Number(guess))){
+      console.log("1");
+      if (number==guess){
+        attempts++;
+        correct_answer=true;
+        alert("Correct Answer!")
+        document.getElementById('guess-output').innerHTML="Number: "+number+"</br>Attempts: "+attempts;
+      }
+      else if(guess>number){
+        attempts++;
+        alert("too high")
+      }
+      else if(guess<number){
+        attempts++;
+        alert("too low")
+      }
+    }
+  }
 
   ////////////////// DO NOT MODIFY
   check('guess'); // DO NOT MODIFY
@@ -240,7 +298,28 @@ function hurricane() {
   ///////////////// DO NOT MODIFY
   let windspeed; // DO NOT MODIFY
   ///////////////// DO NOT MODIFY
-
+  windspeed=Number(prompt('enter your windspeed'));
+  if (windspeed>=157){
+    document.getElementById('hurricane-output').innerHTML='Category 5 Hurricane.';
+  }
+  else if (windspeed>=130){
+    document.getElementById('hurricane-output').innerHTML='Category 4 Hurricane.';
+  }
+  else if (windspeed>=111){
+    document.getElementById('hurricane-output').innerHTML='Category 3 Hurricane.';
+  }
+  else if (windspeed>=96){
+    document.getElementById('hurricane-output').innerHTML='Category 2 Hurricane.';
+  }
+  else if (windspeed>=74){
+    document.getElementById('hurricane-output').innerHTML='Category 1 Hurricane.';
+  }
+  else if (windspeed>=39){
+    document.getElementById('hurricane-output').innerHTML='Tropical Storm.';
+  }
+  else if (windspeed<=38){
+    document.getElementById('hurricane-output').innerHTML='The skies are calm...';
+  }
   ///////////////////////////////// DO NOT MODIFY
   check('hurricane', windspeed); // DO NOT MODIFY
   ///////////////////////////////// DO NOT MODIFY
